5de62944df9678a5106179ec86b675ee7d29c50a
Fernando Antonio GonzÃalez Gaete <fernandogonzalezgaete@gmail.com>
2022-05-24 00:46:14 -0400

    list

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<!-- Este proyecto consiste en un gestor de listas, donde ingresamos nuestras tareas por hacer en un formulario y se muestran seguidamente en una lista después de hacer clic sobre el botón de crear -->
+
+<template>
+  <div id="app">
+    <h1>Crea una nueva tarea</h1>
+    <form @submit.prevent="addTask">
+      <label for="task">Tarea</label>
+      <input
+        type="text"
+        v-model="newTask"
+        placeholder="Ingresa una nueva tarea"
+      />
+      <button type="submit">Crear</button>
+    </form>
+    <h1>Lista</h1>
+    <ul>
+      <li v-for="(task, index) in tasks" :key="index">
+        {{ task.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      tasks: [],
+      newTask: "",
+    };
+  },
+  methods: {
+    addTask() {
+      this.tasks.push({
+        name: this.newTask,
+      });
+      this.newTask = "";
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Arial";
+  font-size: 30px;
+  text-align: center;
+}
+</style>
